8b4f554e655d3cdd3cfe056eaf1a58d948c09667
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-10 15:42:02 +0900

    ９章　htmlからonclickイベントを削除しjsファイルのイベントハンドルに設定するように

--- viewer.js
+++ viewer.js
@@ -1,31 +1,36 @@
-var picNo = 1;
-var myImg = [];
+window.onload = init;
+function init(){
+    var picNo = 1;
+    var myImg = [];
 
-for(var i = 0; i < 4; i++){
-    myImg[i] = document.createElement("img");
-    myImg[i].src = "img/" + ( i + 1 ) + ".jpg";
-}
-function showImg(){
-    document.getElementById("no").innerHTML="No." + picNo;
-    document.getElementById("pict").src=myImg[picNo - 1].src;
-}
-function revBtnClick(){
-    picNo--;
-    showImg();
-    if(picNo === 1){
-        document.getElementById("rev_btn").disabled=true;
-    }else if(picNo === myImg.length - 1){
-        document.getElementById("fwd_btn").disabled=false;
-        document.getElementById("no").className="no1";
+    for(var i = 0; i < 4; i++){
+        myImg[i] = document.createElement("img");
+        myImg[i].src = "img/" + ( i + 1 ) + ".jpg";
     }
-}
-function fwdBtnClick(){
-    picNo++;
-    showImg();
-    if(picNo === myImg.length){
-        document.getElementById("no").className="no2";
-        document.getElementById("fwd_btn").disabled=true;
-    }else if(picNo === 2){
-        document.getElementById("rev_btn").disabled=false;
+    function showImg(){
+        document.getElementById("no").innerHTML="No." + picNo;
+        document.getElementById("pict").src=myImg[picNo - 1].src;
+    }
+    document.getElementById("rev_btn").onclick = revBtnClick;
+    document.getElementById("fwd_btn").onclick = fwdBtnClick;
+    function revBtnClick(){
+        picNo--;
+        showImg();
+        if(picNo === 1){
+            document.getElementById("rev_btn").disabled=true;
+        }else if(picNo === myImg.length - 1){
+            document.getElementById("fwd_btn").disabled=false;
+            document.getElementById("no").className="no1";
+        }
+    }
+    function fwdBtnClick(){
+        picNo++;
+        showImg();
+        if(picNo === myImg.length){
+            document.getElementById("no").className="no2";
+            document.getElementById("fwd_btn").disabled=true;
+        }else if(picNo === 2){
+            document.getElementById("rev_btn").disabled=false;
+        }
     }
 }
