ea84f4b2fcd9c1c5e2fc167d92a66dbe0d4152f1
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-10 14:55:46 +0900

    ９章　２か所でやっている同じ処理を関数にまとめた

--- viewer.js
+++ viewer.js
@@ -5,26 +5,27 @@ for(var i = 0; i < 4; i++){
     myImg[i] = document.createElement("img");
     myImg[i].src = "img/" + ( i + 1 ) + ".jpg";
 }
-
+function showImg(){
+    document.getElementById("no").innerHTML="No." + picNo;
+    document.getElementById("pict").src=myImg[picNo - 1].src;
+}
 function revBtnClick(){
     picNo--;
-    document.getElementById("no").innerHTML="No." + picNo;
+    showImg();
     if(picNo === 1){
         document.getElementById("rev_btn").disabled=true;
     }else if(picNo === 3){
         document.getElementById("fwd_btn").disabled=false;
         document.getElementById("no").className="no1";
     }
-    document.getElementById("pict").src=myImg[picNo - 1].src;
 }
 function fwdBtnClick(){
     picNo++;
-    document.getElementById("no").innerHTML="No." + picNo;
+    showImg();
     if(picNo === 4){
         document.getElementById("no").className="no2";
         document.getElementById("fwd_btn").disabled=true;
     }else if(picNo === 2){
         document.getElementById("rev_btn").disabled=false;
     }
-    document.getElementById("pict").src=myImg[picNo - 1].src;
 }
