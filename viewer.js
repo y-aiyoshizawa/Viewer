14c054e6ce619996e8b9935bb93035bb74000643
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-10 14:19:07 +0900

    ７章　矢印が場合に応じて有効か無効化されるように

--- viewer.js
+++ viewer.js
@@ -2,7 +2,12 @@ var picNo = 1;
 function revBtnClick(){
     picNo--;
     document.getElementById("no").innerHTML="No." + picNo;
-    document.getElementById("no").className="no1";
+    if(picNo === 1){
+        document.getElementById("rev_btn").disabled=true;
+    }else if(picNo === 3){
+        document.getElementById("fwd_btn").disabled=false;
+        document.getElementById("no").className="no1";
+    }
     document.getElementById("pict").src="img/" + picNo + ".jpg";
 }
 function fwdBtnClick(){
@@ -10,6 +15,9 @@ function fwdBtnClick(){
     document.getElementById("no").innerHTML="No." + picNo;
     if(picNo === 4){
         document.getElementById("no").className="no2";
+        document.getElementById("fwd_btn").disabled=true;
+    }else if(picNo === 2){
+        document.getElementById("rev_btn").disabled=false;
     }
     document.getElementById("pict").src="img/" + picNo + ".jpg";
 }
