078bac535584187718895019441e5fd560625784
TTCAD\y-aiyoshizawa <y-aiyoshizawa@ttc-net.co.jp>
2017-05-10 11:53:29 +0900

    ４章　htmlに書いてあったjsをviewer.jsに移行

---
+++ viewer.js
@@ -0,0 +1,10 @@
+function revBtnClick(){
+    document.getElementById("no").innerHTML="No.1";
+    document.getElementById("no").className="no1";
+    document.getElementById("pict").src="img/1.jpg";
+}
+function fwdBtnClick(){
+    document.getElementById("no").innerHTML="No.2";
+    document.getElementById("no").className="no2";
+    document.getElementById("pict").src="img/2.jpg";
+}
